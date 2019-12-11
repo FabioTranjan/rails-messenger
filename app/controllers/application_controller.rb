d1e43cab7888b5620d8c157e5aab7dfe8bc0109a
Fabio Kociolek <fabiotkx@gmail.com>
2019-12-11 17:46:45 -0300

    Used ActiveStorage to edit user image profile.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_devise_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :photo])
   end
 end
