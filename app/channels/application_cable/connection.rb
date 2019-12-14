9ec9fc15fddf85a3e1995264e3a9eb9569c44a2a
Fabio Kociolek <fabiotkx@gmail.com>
2019-12-14 16:44:20 -0300

    Used Action Cable connection to real time chat.

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,19 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verifier_user
+    end
+
+    protected
+
+    def find_verifier_user
+      if verified_user = env["warden"].user
+        verified_user
+      else
+	reject_unauthorized_connection
+      end
+    end
   end
 end
